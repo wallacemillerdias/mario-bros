9d42381c7ff1609f44294e635da415708d7007a6
Wallace Miller <wallacemiller@pop-os.localdomain>
2023-01-24 15:10:47 -0300

    :sparkles: adicionando estrutura inicial

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,20 @@
-alert('oi');
+/*
+
+Objetivo 1 - quando o usuário clicar no botão de veja o trailer, devemos abrir o modal com o vídeo do trailer
+
+Objetivo 2 - quando o usuário clicar no X fechar o modal
+
+Objetivo 1 - quando o usuário clicar no botão de veja o trailer, devemos abrir o modal com o vídeo do trailer
+    - passo 1 - dar um jeito de pegar o elemento que representa o botão da tela usando o js
+    - passo 2 - dar um jeito de identificar quando o usuário clicar no botão
+    - passo 3 - dar um jeito de pegar o elemento da modal no js
+    - passo 4 - abrir o modal na tela
+
+Objetivo 2 - quando o usuário clicar no X fechar o modal
+    - passo 1 - dar um jeito de pegar o elemento de fechar a modal usando o js
+    - passo 2 -  dar um jeito de identificar quando o usuário clica no X
+    - passo 3 -  fechar a modal
+
+ */
+
+console.log(document);
