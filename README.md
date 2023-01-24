dcc1381ea7c0fdb1dd85ed0646bc9bd1c66c2f93
Wallace Miller <wallacemiller@pop-os.localdomain>
2023-01-24 17:09:04 -0300

    :bento: prints

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# projeto-mario
+# Projeto Mario-Bros
+#### Home-page responsiva
+
+aaaaaaaaaaaaaaa.
+
+## Para visualizar:
+
+link-########
+
+## Author
+>Wallace Miller
+>wallllaceeeeee@gmail.com
+> 
+## Protótipos
+![Protótipo 1](https://raw.githubusercontent.com/JeanPaulll/test-for-developer-vacancy-at-texo-it/master/src/assets/prototipos/1.png)
+![Protótipo 2](https://raw.githubusercontent.com/JeanPaulll/test-for-developer-vacancy-at-texo-it/master/src/assets/prototipos/2.png)
+
 
--- src/js/index.js
+++ src/js/index.js
@@ -1,20 +1,19 @@
-/*
+const botaoTrailer = document.querySelector(".botao-trailer");
+const botaoFecharModal = document.querySelector(".fechar-modal");
+const video = document.getElementById("video");
+const modal = document.querySelector(".modal");
+const linkDoVideo = video.src;
 
-Objetivo 1 - quando o usuário clicar no botão de veja o trailer, devemos abrir o modal com o vídeo do trailer
+function alternarModal(){
+	modal.classList.toggle("aberto");
+}
 
-Objetivo 2 - quando o usuário clicar no X fechar o modal
+botaoTrailer.addEventListener("click", () => {
+	alternarModal();
+	video.setAttribute("src", linkDoVideo);
+});
 
-Objetivo 1 - quando o usuário clicar no botão de veja o trailer, devemos abrir o modal com o vídeo do trailer
-    - passo 1 - dar um jeito de pegar o elemento que representa o botão da tela usando o js
-    - passo 2 - dar um jeito de identificar quando o usuário clicar no botão
-    - passo 3 - dar um jeito de pegar o elemento da modal no js
-    - passo 4 - abrir o modal na tela
-
-Objetivo 2 - quando o usuário clicar no X fechar o modal
-    - passo 1 - dar um jeito de pegar o elemento de fechar a modal usando o js
-    - passo 2 -  dar um jeito de identificar quando o usuário clica no X
-    - passo 3 -  fechar a modal
-
- */
-
-console.log(document);
+botaoFecharModal.addEventListener("click", () => {
+	alternarModal();
+	video.setAttribute("src", "");
+});
